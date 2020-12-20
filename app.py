676ce581721da1f19fd7dd7a8119b227621bced6
sele14 <eirik.selander@gmail.com>
2020-12-20 17:02:28 +0100

    app renamed

---
+++ app.py
@@ -0,0 +1,9 @@
+# App entry point.
+from flask_webapp import create_app
+
+def main():
+    app = create_app()
+    app.run(host='127.0.0.1', port=5000, debug=True)
+
+if __name__ == "__main__":
+    main()
